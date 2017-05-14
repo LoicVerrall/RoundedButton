d9a7ad88e87c93569321fe104ad3436211251a94
Loic Verrall <loicverrall@Loics-MacBook-Pro.local>
2017-05-14 20:35:19 +0100

    Initial Commit

---
+++ RoundedButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoundedButton
+//
+//  Created by Loic Verrall on 14/05/2017.
+//  Copyright © 2017 Loic Verrall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
